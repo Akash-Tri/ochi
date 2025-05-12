e3453484e2b1c5964b540d9a3243404488d3fd73
Akash Tripathi <155148204+Akash-Tri@users.noreply.github.com>
2025-05-12 12:11:45 +0530

    Update Cards.jsx ok

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
+import React from 'react';
 
 function Cards() {
   return (
-    <div className='w-full h-screen flex gap-5 items-center px-14'>
+    <div className="w-full min-h-screen flex flex-col md:flex-row gap-5 items-center px-6 md:px-14 py-10">
       
-      <div className="cardcontainer w-1/2 h-[60vh]">
-        <div className='card relative rounded-2xl w-full h-full bg-[#004D43] flex items-center justify-center'>
-          <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
-          <button className='absolute px-5 py-1 rounded-full left-10 bottom-10 border-[1px] text-[#CDEA68]'>&copy;2019-2022</button>
+      {/* Left Card */}
+      <div className="cardcontainer w-full md:w-1/2 h-[50vh] md:h-[60vh]">
+        <div className="card relative rounded-2xl w-full h-full bg-[#004D43] flex items-center justify-center">
+          <img className="w-24 md:w-32" src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
+          <button className="absolute px-4 py-1 text-xs md:text-sm rounded-full left-5 md:left-10 bottom-5 md:bottom-10 border text-[#CDEA68]">
+            &copy; 2019–2022
+          </button>
         </div>
       </div>
 
-      <div className="cardcontainer flex gap-5 w-1/2 h-[60vh]">
-         <div className='card relative rounded-2xl w-1/2 h-full bg-[#192826] flex items-center justify-center '>
-          <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg" alt="" />
-          <button className='absolute px-5 py-1 rounded-full left-10 bottom-10 border-[1px] text-white uppercase'>rating 5.0 on clutch</button>
-         </div>
-         <div className='card relative rounded-2xl w-1/2 h-full bg-[#192826] flex items-center justify-center'>
-          <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo003.png" alt="" />
-          <button className='absolute px-5 py-1 rounded-full left-10 bottom-10 border-[1px] text-white uppercase mr-5'>buisness bootcamp alumini</button>
-         </div>
-      </div>
+      {/* Right Two Cards */}
+      <div className="cardcontainer flex flex-col md:flex-row gap-5 w-full md:w-1/2 h-[100vh] md:h-[60vh]">
+        
+        {/* Top Right Card */}
+        <div className="card relative rounded-2xl w-full md:w-1/2 h-1/2 md:h-full bg-[#192826] flex items-center justify-center">
+          <img className="w-24 md:w-32" src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg" alt="" />
+          <button className="absolute px-4 py-1 text-xs md:text-sm rounded-full left-5 md:left-10 bottom-5 md:bottom-10 border text-white uppercase">
+            Rating 5.0 on Clutch
+          </button>
+        </div>
 
+        {/* Bottom Right Card */}
+        <div className="card relative rounded-2xl w-full md:w-1/2 h-1/2 md:h-full bg-[#192826] flex items-center justify-center">
+          <img className="w-24 md:w-32" src="https://ochi.design/wp-content/uploads/2022/04/logo003.png" alt="" />
+          <button className="absolute px-4 py-1 text-xs md:text-sm rounded-full left-5 md:left-10 bottom-5 md:bottom-10 border text-white uppercase">
+            Business Bootcamp Alumni
+          </button>
+        </div>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
