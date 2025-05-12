7db761e057d5634fdc320297d532516bdb21e43e
Akash Tripathi <155148204+Akash-Tri@users.noreply.github.com>
2025-05-12 12:22:31 +0530

    Update App.jsx ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,50 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import LandingPage from './components/LandingPage'
-import Marque from './components/Marque'
-import About from './components/About'
-import Eyes from './components/Eyes'
-import Featured from './components/Featured'
-import Cards from './components/Cards'
-import Footer from './components/Footer'
+import React, { useEffect } from 'react';
+import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
+import Marque from './components/Marque';
+import About from './components/About';
+import Eyes from './components/Eyes';
+import Featured from './components/Featured';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
 import LocomotiveScroll from 'locomotive-scroll';
 
-function App() { 
-  
+function App() {
+  useEffect(() => {
+    // Initialize Locomotive Scroll
+    const locomotiveScroll = new LocomotiveScroll({
+      el: document.querySelector('.main-container'),
+      smooth: true,
+    });
 
-const locomotiveScroll = new LocomotiveScroll();
+    return () => {
+      // Clean up locomotive scroll instance on unmount
+      locomotiveScroll.destroy();
+    };
+  }, []);
 
   return (
-    <div className='w-full h-screen text-black'>
-      <Navbar/>
-      <LandingPage/>
-      <Marque />
-      <About/>
-      <Eyes />
-      <Featured />
-      <Cards />
-      <Footer />
+    <div className='w-full min-h-screen text-black'>
+      <div className="main-container overflow-hidden">
+        {/* Navbar */}
+        <Navbar />
+        {/* LandingPage */}
+        <LandingPage />
+        {/* Marque */}
+        <Marque />
+        {/* About */}
+        <About />
+        {/* Eyes */}
+        <Eyes />
+        {/* Featured */}
+        <Featured />
+        {/* Cards */}
+        <Cards />
+        {/* Footer */}
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
